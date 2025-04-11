c4ba6465a12bc5337a230c01685e8b7561938f99
Rahan0916 <wk916wk@naver.com>
2025-04-11 18:32:12 +0900

    order api 프론트 적용

---
+++ javawork/kiosk_admin_api/src/main/java/com/kiosk/pay/model/Pay.java
@@ -0,0 +1,38 @@
+package com.kiosk.pay.model;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Pay {
+    private int id;
+    //private int orderId;
+    private int paycode;
+    private String paymentmethod;
+    private String paymentstatus;
+    private int amount;
+    private LocalDateTime paidat;
+}
+
+/*
+localhost:8080/h2-console db
+
+CREATE TABLE pay (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  order_id INT NOT NULL,
+  pay_code INT NOT NULL,
+  payment_method VARCHAR(50) NOT NULL,
+  payment_status VARCHAR(50) NOT NULL,
+  amount INT NOT NULL,
+  paid_at TIMESTAMP NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES orders(id)
+);
+*/
+
