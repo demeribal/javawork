45787f34dc76c3bdb1b6dea92c3bba34739c121f
ksy <iisdf6252@gmail.com>
2025-04-18 11:25:28 +0900

    Kios Session DB이동

--- kiosk_admin_api/src/main/java/com/kiosk/pay/DTO/PayDTO.java
+++ kiosk_admin_api/src/main/java/com/kiosk/pay/DTO/PayDTO.java
@@ -2,15 +2,17 @@ package com.kiosk.pay.DTO;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 
 @Data
 public class PayDTO {
     private int id;                 // 결제 식별자
     private String paymentmethod;  // 결제수단
-    private String paymentstatus;  // 결제내역 (예: 완료, 취소 등)
+    private String paymenthistory;  
     private int amount;            // 결제금액
     private LocalDateTime paidat;  // 결제일
-    private String storelocation;  // 결제지점
-    private String paycode;   
+    private String storeLocation;  // 결제지점
+    private String paycode;
 }
--- kiosk_admin_api/src/main/java/com/kiosk/pay/model/Pay.java
+++ kiosk_admin_api/src/main/java/com/kiosk/pay/model/Pay.java
@@ -13,27 +13,23 @@ import lombok.NoArgsConstructor;
 @Data
 public class Pay {
     private int id;
-    //private int orderId;
     private String paymentmethod;
-    private String paymentstatus;
+    private String paymenthistory;
     private int amount;
-    private int paycode;
+    private String paycode;
     private LocalDateTime paidat;
-    
 }
 
 /*
-localhost:8080/h2-console db
 
 CREATE TABLE pay (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  order_id INT NOT NULL,
-  pay_code INT NOT NULL,
-  payment_method VARCHAR(50) NOT NULL,
-  payment_status VARCHAR(50) NOT NULL,
-  amount INT NOT NULL,
-  paid_at TIMESTAMP NOT NULL,
-  FOREIGN KEY (order_id) REFERENCES orders(id)
+    id INT AUTO_INCREMENT PRIMARY KEY COMMENT '결제 식별자',
+    paymentmethod VARCHAR(50) NOT NULL COMMENT '결제수단',
+    paymenthistory VARCHAR(50) COMMENT '결제 내역',
+    amount INT NOT NULL COMMENT '결제금액',
+    paidat DATETIME COMMENT '결제일',
+    store_location VARCHAR(100) COMMENT '결제지점',
+    paycode VARCHAR(50) COMMENT '결제 코드'
 );
 */
 
