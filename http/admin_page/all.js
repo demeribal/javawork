4aad455cba0675b09b50c42a2ac20502b762986b
unknown <iisdf@naver.com>
2025-04-20 11:45:02 +0900

    favicon 추가

---
+++ http/admin_page/all.js
@@ -0,0 +1,5 @@
+  const favicon = document.createElement("link");
+  favicon.rel = "icon";
+  favicon.href = "/images/administrator.png";
+  favicon.type = "image/png";
+  document.head.appendChild(favicon);
